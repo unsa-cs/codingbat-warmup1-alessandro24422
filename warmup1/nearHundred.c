1c06337ec89cecb617224bf11951f99d65e25a4a
alessandro24422 <asumaria@unsa.edu.pe>
2025-05-16 01:27:39 -0500

    Update nearHundred.c

--- warmup1/nearHundred.c
+++ warmup1/nearHundred.c
@@ -6,6 +6,15 @@
 #include <stdlib.h>
 
 bool nearHundred(int n) {
+    int diff100 = n - 100;
+    if (diff100 < 0) diff100 = -diff100;
+
+    int diff200 = n - 200;
+    if (diff200 < 0) diff200 = -diff200;
+
+    if (diff100 <= 10 || diff200 <= 10) {
+        return true;
+    }
   // TODO: return (abs(n-100) <= 10 || abs(n-200) <= 10);
   return false;
 }
