129d53d5c701fdbebbdd4780eabff0769db6295b
alessandro24422 <asumaria@unsa.edu.pe>
2025-05-16 01:25:29 -0500

    Update missingChar.c

--- warmup1/missingChar.c
+++ warmup1/missingChar.c
@@ -5,6 +5,32 @@
 #include <stdlib.h>
 
 char* missingChar(const char s[], int n) {
+    int len = 0;
+    while (s[len] != '\0') {
+        len++;
+    }
+
+    if (n < 0 || n >= len) {
+        char* result = malloc(len + 1);
+        if (!result) return NULL;
+        for (int i = 0; i <= len; i++) {
+            result[i] = s[i];
+        }
+        return result;
+    }
+
+    char* result = malloc(len);
+    if (!result) return NULL;
+
+    int k = 0;
+    for (int i = 0; i < len; i++) {
+        if (i != n) {
+            result[k++] = s[i];
+        }
+    }
+    result[k] = '\0';
+
+    return result;
   // TODO: implement logic using static buffer or dynamic allocation
   return (char*)s;
 }
