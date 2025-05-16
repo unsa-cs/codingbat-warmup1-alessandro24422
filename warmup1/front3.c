4d1f83d0baa3fc18c52baf341d101e960dad19b0
alessandro24422 <asumaria@unsa.edu.pe>
2025-05-16 01:06:02 -0500

    Update front3.c

--- warmup1/front3.c
+++ warmup1/front3.c
@@ -7,6 +7,27 @@
 #include <stdlib.h>
 
 char* front3(const char s[]) {
+    int len = 0;
+
+    while (s[len] != '\0' && len < 3) {
+        len++;
+    }
+
+    int front_len = (len < 3) ? len : 3;
+
+    char* result = malloc(3 * front_len + 1);
+    if (result == NULL) return NULL;
+
+    int k = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < front_len; j++) {
+            result[k++] = s[j];
+        }
+    }
+
+    result[k] = '\0';
+    return result;
+  
   // TODO: implement logic copying front three times
   return (char*)s;
 }
