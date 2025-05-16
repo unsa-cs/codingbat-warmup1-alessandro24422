35955832ba60212cb101deb35b996ea149e72c8b
alessandro24422 <asumaria@unsa.edu.pe>
2025-05-16 01:21:28 -0500

    Update frontBack.c

--- warmup1/frontBack.c
+++ warmup1/frontBack.c
@@ -5,6 +5,28 @@
 #include <stdlib.h>
 
 char* frontBack(const char s[]) {
+    int len = 0;
+    while (s[len] != '\0') {
+        len++;
+    }
+    if (len <= 1) {
+        char* result = malloc(len + 1);
+        if (result == NULL) return NULL;
+        for (int i = 0; i <= len; i++) {
+            result[i] = s[i];
+        }
+        return result;
+    }
+    char* result = malloc(len + 1);
+    if (result == NULL) return NULL;
+    for (int i = 0; i < len; i++) {
+        result[i] = s[i];
+    }
+    result[len] = '\0';
+    char temp = result[0];
+    result[0] = result[len - 1];
+    result[len - 1] = temp;
+    return result;
   // TODO: implement swapping first and last character
   return (char*)s;
 }
