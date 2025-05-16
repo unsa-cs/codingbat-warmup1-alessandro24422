985c8aea6877d09a229823f8eb8853049938b955
alessandro24422 <asumaria@unsa.edu.pe>
2025-05-16 01:29:12 -0500

    Update notString.c

--- warmup1/notString.c
+++ warmup1/notString.c
@@ -6,6 +6,33 @@
 #include <stdlib.h>
 
 char* notString(const char s[]) {
+    int len = 0;
+    while (s[len] != '\0') {
+        len++;
+    }
+
+    if (len >= 3 && s[0] == 'n' && s[1] == 'o' && s[2] == 't') {
+        char* result = malloc(len + 1);
+        if (!result) return NULL;
+        for (int i = 0; i <= len; i++) {
+            result[i] = s[i];
+        }
+        return result;
+    }
+
+    char* result = malloc(len + 5);
+    if (!result) return NULL;
+
+    result[0] = 'n';
+    result[1] = 'o';
+    result[2] = 't';
+    result[3] = ' ';
+
+    for (int i = 0; i <= len; i++) {
+        result[i + 4] = s[i];
+    }
+
+    return result;
   // TODO: implement logic using static buffer or dynamic allocation
   return (char*)s;
 }
